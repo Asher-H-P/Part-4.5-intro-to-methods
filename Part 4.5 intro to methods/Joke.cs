fac6615209ca815ed4bceaa8a62713d24172cafa
Asher Heywood Peters <339513541@GoTVDSB.ca>
2023-09-22 10:50:14 -0400

    Add project files.

---
+++ Part 4.5 intro to methods/Joke.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Part_4._5_intro_to_methods
+{
+    public class Joke
+    {
+        public Joke() 
+        {
+            Console.WriteLine("99 little bugs in the code");
+            Thread.Sleep(500);
+            Console.WriteLine("99 little bugs.");
+            Thread.Sleep(500);
+            Console.WriteLine("Fix a bug, run it again,");
+            Thread.Sleep(500);
+            Console.WriteLine("100 little bugs in the code.");
+        }
+    }
+}
---
+++ Part 4.5 intro to methods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Part_4._5_intro_to_methods
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Give me a number.");
+            num = Convert.ToInt32(Console.ReadLine());
+            Joke(num);
+        }
+        public static void Joke(int numBugs)
+        {
+            Console.WriteLine(numBugs + " little bugs in the code");
+            Thread.Sleep(1000);
+            Console.WriteLine(numBugs + " little bugs.");
+            Thread.Sleep(1000);
+            Console.WriteLine("Fix a bug, run it again,");
+            Thread.Sleep(1000);
+            Console.WriteLine((++numBugs) + " little bugs in the code.");
+        }
+    }
+}
